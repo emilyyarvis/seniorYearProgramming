42e33687b15dd57ce3d2ba6db369b1ac0fc07e57
emilyyarvis <395410@bsd48.org>
2022-12-13 10:12:33 -0800

    I figured out how maps work

--- room.cpp
+++ room.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include"room.h"
-
+#include <map>
 
 using namespace std;
 
@@ -8,8 +8,26 @@ room::room(string description){
   roomDescription = description;
 }
 
+string room::getRoomDescription(){
+  return roomDescription;
+}
 void room::setItem(item newItem){
 
   itemList.push_back(newItem);
-
 }
+
+void room::setExit(string direction,int roomIndex){
+  exits.insert(std::pair<string, int>(direction,roomIndex));
+}
+
+void room::getExits(){
+  int index = exits.size();
+  map<string, int>::iterator i;
+
+  
+  for (i = exits.begin(); i != exits.end(); i++) {
+    cout << i->first << ", "<< i->second<<endl;
+  }
+}
+
+  
--- zuulConsole.cpp
+++ zuulConsole.cpp
@@ -2,19 +2,47 @@
 #include "room.h"
 //#include "item.h"
 
+
 using namespace std;
+
 
 int main(){
 
   bool loop;
   char userInput[100];
+  //create Rooms
+  room* mainHall = new room("Look my first room");
+  room* bounceyHouse = new room("This is a room");
+  room* parlor = new room("this is the parlor");
+  cout<<mainHall->getRoomDescription()<<endl;
+  //
+  mainHall->setExit("North", 2);
+  mainHall->setExit("West",1);
+  mainHall->getExits();
+  
+  // bounceyHouse->setExit("North", 20);
+  // bounceyHouse->setExit("West", 40);
+  //bounceyHouse->getExits();
+
+
+  //
+  cout<<"did it work"<<endl;
   while(loop == true){
     cout<<"Insert instructions here"<<endl;
     cin.get(userInput,100);
-
-   
-
-
-
+    if(strcmp(userInput,"EXIT")==0){
+      loop = false;
+    }
+    else if(strcmp(userInput,"GO")==0){
+      cout<<"something soon"<<endl;
+    }
+    else if(strcmp(userInput, "GET")==0){
+	cout<<"somethig soon"<<endl;
+      }
+    else{
+      cout<<"thta is not a valid command"<<endl;
+    }
   }
 }
+
+
